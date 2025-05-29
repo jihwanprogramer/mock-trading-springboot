5c90c0d1610bb48b9c642ac44b46b08e9a0e079e
RKS <rudtjs5719@gmail.com>
2025-05-29 16:31:21 +0900

    fix : 오타수정

--- src/main/java/com/example/mockstalk/domain/comment/controller/CommentController.java
+++ src/main/java/com/example/mockstalk/domain/comment/controller/CommentController.java
@@ -1,7 +1,12 @@
 package com.example.mockstalk.domain.comment.controller;
 
-import static org.springframework.data.domain.Sort.Direction.*;
 
+import static org.springframework.data.domain.Sort.Direction.DESC;
+
+import com.example.mockstalk.common.response.ResponseMessage;
+import com.example.mockstalk.domain.comment.dto.CommentRequestDto;
+import com.example.mockstalk.domain.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -17,46 +22,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.mockstalk.common.response.ResponseMessage;
-import com.example.mockstalk.domain.comment.dto.CommentRequestDto;
-import com.example.mockstalk.domain.comment.service.CommentService;
-
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping("/boards")
 @RequiredArgsConstructor
 public class CommentController {
 
-	private final CommentService commentService;
+    private final CommentService commentService;
 
-	@PostMapping("/{boardId}/comments")
-	public ResponseEntity<ResponseMessage<?>> saveComment(
-		@AuthenticationPrincipal UserDetails userDetails,
-		@PathVariable Long boardId,
-		@RequestBody CommentRequestDto commentRequestDto
-	) {
-		commentService.saveComment(userDetails, boardId, commentRequestDto);
-		return ResponseEntity.status(HttpStatus.CREATED).body(ResponseMessage.success());
-	}
+    @PostMapping("/{boardId}/comments")
+    public ResponseEntity<ResponseMessage<?>> saveComment(
+        @AuthenticationPrincipal UserDetails userDetails,
+        @PathVariable Long boardId,
+        @RequestBody CommentRequestDto commentRequestDto
+    ) {
+        commentService.saveComment(userDetails, boardId, commentRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseMessage.success());
+    }
 
-	@GetMapping("/{boardId}/comments")
-	public ResponseEntity<ResponseMessage<?>> findBoardByBoardId(
-		@PathVariable Long boardId,
-		@PageableDefault(size = 10, sort = "creatTime", direction = DESC) Pageable pageable) {
-		return ResponseEntity.ok(
-			ResponseMessage.success(commentService.findCommentByBoardId(boardId, pageable)));
-	}
+    @GetMapping("/{boardId}/comments")
+    public ResponseEntity<ResponseMessage<?>> findBoardByBoardId(
+        @PathVariable Long boardId,
+        @PageableDefault(size = 10, sort = "creatTime", direction = DESC) Pageable pageable) {
+        return ResponseEntity.ok(
+            ResponseMessage.success(commentService.findCommentByBoardId(boardId, pageable)));
+    }
 
-	@PatchMapping("/{boardId}/comments/{commentId}")
-	public ResponseEntity<ResponseMessage<?>> updateComment(
-		@AuthenticationPrincipal UserDetails userDetails,
-		@PathVariable Long boardId,
-		@PathVariable Long commentId,
-		@RequestBody CommentRequestDto commentRequestDto) {
-		commentService.updateComment(userDetails, boardId, commentId, commentRequestDto);
-		return ResponseEntity.ok(ResponseMessage.success("댓글이 수정되었습니다."));
-	}
+    @PatchMapping("/{boardId}/comments/{commentId}")
+    public ResponseEntity<ResponseMessage<?>> updateComment(
+        @AuthenticationPrincipal UserDetails userDetails,
+        @PathVariable Long boardId,
+        @PathVariable Long commentId,
+        @RequestBody CommentRequestDto commentRequestDto) {
+        commentService.updateComment(userDetails, boardId, commentId, commentRequestDto);
+        return ResponseEntity.ok(ResponseMessage.success("댓글이 수정되었습니다."));
+    }
 
 	@DeleteMapping("/{boardId}/comments/{commentId}")
 	public ResponseEntity<ResponseMessage<?>> deleteComment(
