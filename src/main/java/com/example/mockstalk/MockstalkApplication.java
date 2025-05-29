8b3d3a453b7f8a7851a27826432c3b6dea7e8887
dikaprio1 <159988070+dikaprio1@users.noreply.github.com>
2025-05-29 18:59:58 +0900

    feat : 네이버 검색 api연동
    키워드를 통해 관련 뉴스 검색
    디비에 직접 저장하기보단
    레디스 캐싱을 이용해서 처리

--- src/main/java/com/example/mockstalk/MockstalkApplication.java
+++ src/main/java/com/example/mockstalk/MockstalkApplication.java
@@ -2,14 +2,16 @@ package com.example.mockstalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching //레디스 캐싱을 활요하기위해 활성화
 public class MockstalkApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MockstalkApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MockstalkApplication.class, args);
+	}
 
 }
