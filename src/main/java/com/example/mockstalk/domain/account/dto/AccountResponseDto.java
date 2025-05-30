15a0f127fda585cadda920a12bf25b538d713a14
rzbobo <127168779+rzbobo@users.noreply.github.com>
2025-05-30 21:37:24 +0900

    Feat : CRUD 조회 제외 기본 MVP 기능

--- src/main/java/com/example/mockstalk/domain/account/dto/AccountResponseDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.mockstalk.domain.account.dto;
-
-public class AccountResponseDto {
-}
--- src/main/java/com/example/mockstalk/domain/account/service/AccountService.java
+++ src/main/java/com/example/mockstalk/domain/account/service/AccountService.java
@@ -1,8 +1,15 @@
 package com.example.mockstalk.domain.account.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.mockstalk.common.error.CustomRuntimeException;
+import com.example.mockstalk.common.error.ExceptionCode;
+import com.example.mockstalk.domain.account.dto.AccountRequestDto;
+import com.example.mockstalk.domain.account.dto.UpdateAccountRequestDto;
+import com.example.mockstalk.domain.account.entity.Account;
 import com.example.mockstalk.domain.account.repository.AccountRepository;
+import com.example.mockstalk.domain.holdings.repository.HoldingsRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,8 +19,50 @@ public class AccountService {
 
 	private final AccountRepository accountRepository;
 
-	public void saveAccount() {
+	private final HoldingsRepository holdingsRepository;
 
+	@Transactional
+	public void saveAccount(AccountRequestDto accountRequestDto) {
+		Account account = Account.builder()
+			.accountName(accountRequestDto.getAccountName())
+			.password(accountRequestDto.getPassword())
+			.initialBalance(accountRequestDto.getInitialBalance())
+			.build();
+		accountRepository.save(account);
+	}
+
+	@Transactional(readOnly = true)
+	public void findAccountById(Long id) {
+
+		Account account = accountRepository.findById(id).
+			orElseThrow(() -> new CustomRuntimeException(ExceptionCode.NOT_FOUND_ACCOUNT));
+
+	}
+
+	@Transactional(readOnly = true)
+	public void findAccount() {
+
+	}
+
+	public void findHoldingsById(Long id) {
+		// 보유종목 리스트 조회
+	}
+
+	@Transactional
+	public void updateAccountPassword(Long id, UpdateAccountRequestDto requestDto) {
+		Account account = accountRepository.findById(id).
+			orElseThrow(() -> new CustomRuntimeException(ExceptionCode.NOT_FOUND_ACCOUNT));
+
+		account.updatePassword(requestDto.getPassword());
+
+	}
+
+	@Transactional
+	public void deleteAccount(Long id) {
+		Account account = accountRepository.findById(id).
+			orElseThrow(() -> new CustomRuntimeException(ExceptionCode.NOT_FOUND_ACCOUNT));
+
+		accountRepository.delete(account);
 	}
 
 }
