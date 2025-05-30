717f9050e3ba1dee2e1d16023dc228b88c31a6f7
ehllll <leeeunhye0510@naver.com>
2025-05-30 12:04:59 +0900

    style: 테스트시 주석처리 해놨던거 해제

--- src/main/java/com/example/mockstalk/domain/comment/service/CommentService.java
+++ src/main/java/com/example/mockstalk/domain/comment/service/CommentService.java
@@ -1,5 +1,10 @@
-/*
 package com.example.mockstalk.domain.comment.service;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.mockstalk.common.error.CustomRuntimeException;
 import com.example.mockstalk.common.error.ExceptionCode;
@@ -9,67 +14,61 @@ import com.example.mockstalk.domain.comment.dto.CommentResponseDto;
 import com.example.mockstalk.domain.comment.entity.Comment;
 import com.example.mockstalk.domain.comment.repository.CommentRepository;
 import com.example.mockstalk.domain.user.repository.UserRepository;
+
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
 public class CommentService {
 
-    private final CommentRepository commentRepository;
-    private final BoardRepository boardRepository;
-    private final UserRepository userRepository;
+	private final CommentRepository commentRepository;
+	private final BoardRepository boardRepository;
+	private final UserRepository userRepository;
 
-    @Transactional
-    public void saveComment(UserDetails userDetails, Long boardId,
-        CommentRequestDto commentRequestDto) {
+	@Transactional
+	public void saveComment(UserDetails userDetails, Long boardId,
+		CommentRequestDto commentRequestDto) {
 
-        Comment comment = Comment.builder()
-            .content(commentRequestDto.getContent())
-            .board(boardRepository.findById(boardId).orElseThrow())
-            .user(userRepository.findById(Long.parseLong(userDetails.getUsername())).orElseThrow())
-            .build();
+		Comment comment = Comment.builder()
+			.content(commentRequestDto.getContent())
+			.board(boardRepository.findById(boardId).orElseThrow())
+			.user(userRepository.findById(Long.parseLong(userDetails.getUsername())).orElseThrow())
+			.build();
 
-        commentRepository.save(comment);
-    }
+		commentRepository.save(comment);
+	}
 
-    @Transactional(readOnly = true)
-    public Slice<CommentResponseDto> findCommentByBoardId(Long boardId, Pageable pageable) {
-        return commentRepository.findAllByBoardId(boardId, pageable).map(CommentResponseDto::from);
-    }
+	@Transactional(readOnly = true)
+	public Slice<CommentResponseDto> findCommentByBoardId(Long boardId, Pageable pageable) {
+		return commentRepository.findAllByBoardId(boardId, pageable).map(CommentResponseDto::from);
+	}
 
-    @Transactional
-    public void updateComment(UserDetails userDetails, Long boardId, Long commentId,
-        CommentRequestDto commentRequestDto) {
-        Comment comment = validComment(userDetails, boardId, commentId);
-        comment.update(commentRequestDto.getContent());
-    }
-    
-    @Transactional
-    public void deleteComment(UserDetails userDetails, Long boardId, Long commentId) {
-        Comment comment = validComment(userDetails, boardId, commentId);
-        commentRepository.delete(comment);
-    }
+	@Transactional
+	public void updateComment(UserDetails userDetails, Long boardId, Long commentId,
+		CommentRequestDto commentRequestDto) {
+		Comment comment = validComment(userDetails, boardId, commentId);
+		comment.update(commentRequestDto.getContent());
+	}
 
-    public Comment validComment(UserDetails userDetails, Long boardId, Long commentId) {
-        Comment comment = commentRepository.findById(commentId)
-            .orElseThrow(() -> new CustomRuntimeException(ExceptionCode.NOT_FOUND_COMMENT));
+	@Transactional
+	public void deleteComment(UserDetails userDetails, Long boardId, Long commentId) {
+		Comment comment = validComment(userDetails, boardId, commentId);
+		commentRepository.delete(comment);
+	}
 
-        if (!comment.getBoard().getId().equals(boardId)) {
-            throw new CustomRuntimeException(ExceptionCode.COMMENT_MISMATCH_EXCEPTION);
-        }
+	public Comment validComment(UserDetails userDetails, Long boardId, Long commentId) {
+		Comment comment = commentRepository.findById(commentId)
+			.orElseThrow(() -> new CustomRuntimeException(ExceptionCode.NOT_FOUND_COMMENT));
 
-        if (!userDetails.getUsername().equals(comment.getUser().getId())) {
-            throw new CustomRuntimeException(ExceptionCode.USER_MISMATCH_EXCEPTION);
-        }
+		if (!comment.getBoard().getId().equals(boardId)) {
+			throw new CustomRuntimeException(ExceptionCode.COMMENT_MISMATCH_EXCEPTION);
+		}
 
-        return comment;
-    }
+		if (!userDetails.getUsername().equals(comment.getUser().getId())) {
+			throw new CustomRuntimeException(ExceptionCode.USER_MISMATCH_EXCEPTION);
+		}
 
+		return comment;
+	}
 
 }
-*/
