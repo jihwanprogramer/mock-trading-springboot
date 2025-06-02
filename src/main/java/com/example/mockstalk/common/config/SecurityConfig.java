78dcce82c92c9122a0754ba44b0dbdd5e4cbe2ce
kdongyeon <91926630+kdongyeon@users.noreply.github.com>
2025-06-02 19:34:36 +0900

    feat : 유저디테일인증 객체등록

--- src/main/java/com/example/mockstalk/common/config/SecurityConfig.java
+++ src/main/java/com/example/mockstalk/common/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,6 +17,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -24,7 +27,7 @@ public class SecurityConfig {
 
     @Bean
     public JwtFilter jwtFilter() {
-        return new JwtFilter(jwtUtil);  // JwtFilter 빈 등록
+        return new JwtFilter(jwtUtil, userDetailsService);  // JwtFilter 빈 등록
     }
 
     @Bean
@@ -33,6 +36,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(user -> user
                         .requestMatchers("/users/login", "/users/signup").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
