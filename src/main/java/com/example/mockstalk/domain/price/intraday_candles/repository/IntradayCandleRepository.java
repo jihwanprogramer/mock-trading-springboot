e0c06fa02bbbab9e016aaa7f603bf512b72c10b4
ehllll <leeeunhye0510@naver.com>
2025-06-04 15:58:20 +0900

    style: 분봉 캔들차트 구현중코드 주석처리

---
+++ src/main/java/com/example/mockstalk/domain/price/intraday_candles/repository/IntradayCandleRepository.java
@@ -0,0 +1,17 @@
+/*
+package com.example.mockstalk.domain.price.intraday_candles.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.mockstalk.domain.price.intraday_candles.entity.IntradayCandle;
+
+@Repository
+public interface IntradayCandleRepository extends JpaRepository<IntradayCandle, Long> {
+
+	List<IntradayCandle> findByStockCodeAndTimeStampBetween(String stockCode, LocalDateTime start, LocalDateTime end);
+}
+*/
