223e9502b52bc3a52a1097eb4c65d3e6d32b2020
RKS <rudtjs5719@gmail.com>
2025-06-04 14:34:32 +0900

    feat : 주식 응답 구현 및 엔티티 수정

--- src/main/java/com/example/mockstalk/domain/price/periodic_candles/repository/CandleSticksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mockstalk.domain.price.periodic_candles.repository;
-
-import com.example.mockstalk.domain.price.periodic_candles.entity.PeriodicCandles;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CandleSticksRepository extends JpaRepository<PeriodicCandles,Long> {
-}
