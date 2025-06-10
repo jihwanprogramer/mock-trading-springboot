8b1bf7624f7d871ec59c7b5b556b033510f44805
jihwan <kob882333@naver.com>
2025-06-10 20:54:23 +0900

    fix:실시간 종목조회x 프로그램 실행되기는함

--- src/main/java/com/example/mockstalk/domain/price/periodic_candles/repository/PeriodicCandleRepository.java
+++ src/main/java/com/example/mockstalk/domain/price/periodic_candles/repository/PeriodicCandleRepository.java
@@ -1,14 +1,16 @@
 package com.example.mockstalk.domain.price.periodic_candles.repository;
 
-import com.example.mockstalk.domain.price.periodic_candles.entity.PeriodicCandleType;
-import com.example.mockstalk.domain.price.periodic_candles.entity.PeriodicCandles;
 import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.mockstalk.domain.price.periodic_candles.entity.PeriodicCandleType;
+import com.example.mockstalk.domain.price.periodic_candles.entity.PeriodicCandles;
 
 @Repository
 public interface PeriodicCandleRepository extends JpaRepository<PeriodicCandles, Long> {
 
-    Optional<PeriodicCandles> findByCandleTypeAndStockCode(PeriodicCandleType candleType,
-        String stockCode);
+	Optional<PeriodicCandles> findByCandleTypeAndStock_StockCode(PeriodicCandleType candleType,
+		String stockCode);
 }
