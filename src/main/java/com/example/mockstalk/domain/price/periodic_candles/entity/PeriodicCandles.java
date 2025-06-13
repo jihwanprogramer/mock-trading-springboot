6665455d5302325d2c5adb6b116fcbf79e6d1e3d
RKS <rudtjs5719@gmail.com>
2025-06-13 14:37:38 +0900

    feat : api 호출 방식 수정 및 적용

--- src/main/java/com/example/mockstalk/domain/price/periodic_candles/entity/PeriodicCandles.java
+++ src/main/java/com/example/mockstalk/domain/price/periodic_candles/entity/PeriodicCandles.java
@@ -6,10 +6,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,12 +22,16 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @Builder
-@Table(name = "periodic_candles")
+@Table(
+    name = "periodic_candles",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"stock_id", "candle_type", "date"})
+)
 @NoArgsConstructor
 @AllArgsConstructor
 public class PeriodicCandles extends BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
