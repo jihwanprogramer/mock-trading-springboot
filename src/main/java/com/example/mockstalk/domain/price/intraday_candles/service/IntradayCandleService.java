af1763b3a3382df57a531108342a41f52ffce927
ehllll <leeeunhye0510@naver.com>
2025-06-16 17:32:37 +0900

    feat: 5분브리핑 피드백 반영 ( @Slf4j 적용,실패종목 재시도큐 도입, 중복캔들 데이터 upsert 처리방식으로 변경)

--- src/main/java/com/example/mockstalk/domain/price/intraday_candles/service/IntradayCandleService.java
+++ src/main/java/com/example/mockstalk/domain/price/intraday_candles/service/IntradayCandleService.java
@@ -3,8 +3,12 @@ package com.example.mockstalk.domain.price.intraday_candles.service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -26,15 +30,18 @@ import com.example.mockstalk.domain.stock.repository.StockRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class IntradayCandleService {
 
 	private final RestTemplate restTemplate;
 	private final IntradayCandleRepository intradayCandleRepository;
 	private final TokenService tokenService;
 	private final StockRepository stockRepository;
+	private final Queue<String> errorQueue = new ConcurrentLinkedQueue<>();
 
 	@Value("${hantu-openapi.appkey}")
 	private String appKey;
@@ -56,7 +63,21 @@ public class IntradayCandleService {
 			try {
 				fetchAndSaveIntradayCandles(stockCode);  // 각각의 종목코드에 대해 분봉 데이터 수집
 			} catch (Exception e) {
-				System.err.println(stockCode + "종목 처리 중 오류 발생: " + e.getMessage());
+				log.error("{} 종목 처리 중 오류 발생: {}", stockCode, e.getMessage());
+				errorQueue.add(stockCode);
+			}
+		}
+	}
+
+	@Scheduled(cron = "0 30 9,11,13 * * MON-FRI")
+	public void retryFaildStocks() {
+		while (!errorQueue.isEmpty()) {
+			String failedStockCode = errorQueue.poll();
+			try {
+				fetchAndSaveIntradayCandles(failedStockCode);
+				log.info("재시도 성공: {}", failedStockCode);
+			} catch (Exception e) {
+				log.error("재시도 실패: {} - {}", failedStockCode, e.getMessage());
 			}
 		}
 	}
@@ -71,7 +92,7 @@ public class IntradayCandleService {
 			List<String> startTimes = getStartTimesByInterval(interval);
 
 			for (String startTime : startTimes) {
-				System.out.println("종목코드: " + stockCode + ", 인터벌: " + interval + ", 시작시간: " + startTime);
+				log.info("종목코드: {}, 인터벌: {}, 시작시간: {}", stockCode, interval, startTime);
 
 				String url = buildApiUrl(stockCode, startTime);
 				HttpHeaders headers = createHeaders(token);
@@ -79,13 +100,58 @@ public class IntradayCandleService {
 				try {
 					ResponseEntity<Map> response = sendApiRequest(url, headers);
 
-					saveCandlesIfNotExist(response, stockCode);
+					saveOrUpdateCandles(response, stockCode);
 
 				} catch (Exception e) {
-					System.err.println("API 호출 실패: " + e.getMessage());
+					log.error("API 호출 실패: {}-{}", stockCode, e.getMessage());
 				}
 			}
 		}
+	}
+
+	private void saveOrUpdateCandles(ResponseEntity<Map> response, String stockCode) {
+
+		List<Map<String, Object>> output2 = (List<Map<String, Object>>)response.getBody().get("output2");
+
+		List<IntradayCandleApiResponseDto> dtoList = output2.stream()
+			.map(data -> new IntradayCandleApiResponseDto(
+				(String)data.get("stck_bsop_date"),
+				(String)data.get("stck_cntg_hour"),
+				String.valueOf(data.get("stck_prpr")),
+				String.valueOf(data.get("stck_oprc")),
+				String.valueOf(data.get("stck_hgpr")),
+				String.valueOf(data.get("stck_lwpr")),
+				String.valueOf(data.get("cntg_vol")),
+				String.valueOf(data.get("acml_tr_pbmn"))
+			))
+			.toList();
+
+		Stock stock = stockRepository.findByStockCode(stockCode);
+		if (stock == null) {
+			log.error("stock 엔티티를 찾을 수 없습니다: {} ", stockCode);
+			return;
+		}
+
+		List<IntradayCandle> newCandles = new ArrayList<>();
+		for (IntradayCandleApiResponseDto dto : dtoList) {
+			LocalDateTime timestamp = LocalDateTime.parse(
+				dto.getStck_bsop_date() + dto.getStck_cntg_hour(),
+				DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+			);
+
+			Optional<IntradayCandle> existing = intradayCandleRepository
+				.findByStock_StockCodeAndTimeStamp(stockCode, timestamp);
+
+			IntradayCandle entity = dto.toEntity(stock);
+
+			if (existing.isPresent()) {
+				IntradayCandle candle = existing.get();
+				candle.updateFrom(entity);
+			} else {
+				newCandles.add(entity);
+			}
+		}
+		intradayCandleRepository.saveAll(newCandles);
 	}
 
 	private String buildApiUrl(String stockCode, String startTime) {
@@ -113,37 +179,6 @@ public class IntradayCandleService {
 	private ResponseEntity<Map> sendApiRequest(String url, HttpHeaders headers) {
 		HttpEntity<Void> entity = new HttpEntity<>(headers);
 		return restTemplate.exchange(url, HttpMethod.GET, entity, Map.class);
-	}
-
-	private void saveCandlesIfNotExist(ResponseEntity<Map> response, String stockCode) {
-
-		List<Map<String, Object>> output2 = (List<Map<String, Object>>)response.getBody().get("output2");
-		List<IntradayCandleApiResponseDto> dtoList = output2.stream()
-			.map(data -> new IntradayCandleApiResponseDto(
-				(String)data.get("stck_bsop_date"),
-				(String)data.get("stck_cntg_hour"),
-				String.valueOf(data.get("stck_prpr")),
-				String.valueOf(data.get("stck_oprc")),
-				String.valueOf(data.get("stck_hgpr")),
-				String.valueOf(data.get("stck_lwpr")),
-				String.valueOf(data.get("cntg_vol")),
-				String.valueOf(data.get("acml_tr_pbmn"))
-			))
-			.toList();
-
-		Stock stock = stockRepository.findByStockCode(stockCode);
-		if (stock == null) {
-			System.err.println("stock 엔티티를 찾을 수 없습니다: " + stockCode);
-			return;
-		}
-		try {
-			List<IntradayCandle> candleEntityList =
-				dtoList.stream().map(dto -> dto.toEntity(stock)).toList();
-			intradayCandleRepository.saveAll(candleEntityList);
-		} catch (Exception e) {
-			System.err.println("개별 캔들 저장 실패: " + e.getMessage());
-		}
-
 	}
 
 	private List<String> getStartTimesByInterval(String interval) {
@@ -174,7 +209,7 @@ public class IntradayCandleService {
 			);
 
 		} catch (Exception e) {
-			System.err.println("조회 실패: " + e.getMessage());
+			log.error("조회 실패: {}-{}", stockCode, e.getMessage());
 			return List.of();
 		}
 	}
