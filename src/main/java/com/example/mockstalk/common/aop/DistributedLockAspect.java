1ae3fb307a7c338836784d4e21c72508f6615ef7
jihwan <kob882333@naver.com>
2025-06-18 20:55:09 +0900

    feat:트랜잭션 수동 커밋로직 추가

--- src/main/java/com/example/mockstalk/common/aop/DistributedLockAspect.java
+++ src/main/java/com/example/mockstalk/common/aop/DistributedLockAspect.java
@@ -8,6 +8,9 @@ import org.aspectj.lang.annotation.Aspect;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 public class DistributedLockAspect {
 
 	private final RedissonClient redissonClient;
+	private final PlatformTransactionManager transactionManager;
 
 	@Around("@annotation(distributedLock)")
 	public Object lock(ProceedingJoinPoint joinPoint, DistributedLock distributedLock) throws Throwable {
@@ -32,7 +36,19 @@ public class DistributedLockAspect {
 			if (!isLocked) {
 				throw new IllegalStateException("락 획득 실패: key = " + key);
 			}
-			return joinPoint.proceed(); // 비즈니스 로직 실행
+
+			TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+			transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+			return transactionTemplate.execute(status -> {
+				try {
+					return joinPoint.proceed(); // 비즈니스 로직 실행
+				} catch (Throwable throwable) {
+					// 트랜잭션 롤백을 명시적으로 설정
+					status.setRollbackOnly();
+					throw new RuntimeException(throwable);
+				}
+			}); // 비즈니스 로직 실행
 		} finally {
 			if (isLocked && lock.isHeldByCurrentThread()) {
 				lock.unlock();
