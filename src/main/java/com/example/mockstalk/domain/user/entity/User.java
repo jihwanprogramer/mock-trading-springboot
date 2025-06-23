d1d0879a8d1b62a1d3dd3e75b4d0b94a89689974
jihwan <kob882333@naver.com>
2025-06-23 12:51:33 +0900

    feat:빌더추가

--- src/main/java/com/example/mockstalk/domain/user/entity/User.java
+++ src/main/java/com/example/mockstalk/domain/user/entity/User.java
@@ -19,12 +19,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
 @Table(name = "users")
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User extends BaseEntity {
