8a5671c6b241f6d5619a54f6fcb931e2d2387fde
dikaprio1 <159988070+dikaprio1@users.noreply.github.com>
2025-07-07 02:30:38 +0900

    feat : 가격정보 api

---
+++ src/main/java/com/example/mockstalk/domain/price/livePrice/controller/LivePriceController.java
@@ -0,0 +1,25 @@
+package com.example.mockstalk.domain.price.livePrice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mockstalk.domain.price.livePrice.service.LivePriceService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class LivePriceController {
+
+	private final LivePriceService livePriceService;
+
+	@GetMapping("/price")
+	public ResponseEntity<String> getPriceByStockName(@RequestParam String stockName) {
+		String price = livePriceService.getCurrentPriceByStockName(stockName);
+		return ResponseEntity.ok(price);
+	}
+}
